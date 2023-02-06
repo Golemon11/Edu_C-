5f664e04ba77dc30f9f078f122c190bf7e09b50a
Golemon11 <oleg.ru00@gmail.com>
2023-02-06 19:27:36 +0300

    end1234

--- Task1/sem3.cs
+++ Task1/sem3.cs
@@ -1,73 +1,81 @@
-/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
-
-*/
+/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
 /*
-int[] myArray = new int[5];
-Random rand = new Random();
-int count = 0;
-for (int i = 0; i < myArray.Length; i++)
-    myArray[i] = rand.Next(100, 300);
-
-for (int z = 0; z < myArray.Length; z++)
-if (myArray[z] % 2 == 0)
-count++;
-void PrintArray(int[] myArray)
-{
-    Console.Write("[ ");
-    for(int i = 0; i < myArray.Length; i++)
-    {
-        Console.Write(myArray[i] + " ");
-    }
-    Console.Write("]");
-    Console.WriteLine();
-}
-PrintArray(myArray);
-Console.WriteLine($"Количество чётных чисел в массиве: {count}");
-*/
-/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.*/
-/*
-int[] myArray = new int[5];
-Random rand = new Random();
+Console.Write("Введите числа через запятую: ");
+int[] numbers = StringToNum(Console.ReadLine());
+PrintArray(numbers);
 int sum = 0;
-for (int i = 0; i < myArray.Length; i++)
-    myArray[i] = rand.Next(100, 300);
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {sum}");
 
-for (int z = 0; z < myArray.Length; z+=2)
-    sum = sum + myArray[z];
-void PrintArray(int[] myArray)
+
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ',')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+
+
+void PrintArray(int[] array)
 {
     Console.Write("[ ");
-    for(int i = 0; i < myArray.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(myArray[i] + " ");
+        Console.Write(array[i] + " ");
     }
     Console.Write("]");
-    Console.WriteLine();
 }
-PrintArray(myArray);
- Console.WriteLine($"всего {myArray.Length} чисел, сумма элементов на нечётных позициях = {sum}");
- */
- /*Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.*/
+*/
+/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.*/
 /*
-int[] myArray = new int[5];
- Random rand = new Random();
-for (int i = 0; i < myArray.Length; i++)
-    myArray[i] = rand.Next(100, 300);
-double min = Int32.MaxValue;
-double max = Int32.MinValue;
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
 
-for (int z = 0; z < myArray.Length; z++)
-{
-    if (myArray[z] > max)
-        {
-            max = myArray[z];
-        }
-    if (myArray[z] < min)
-        {
-            min = myArray[z];
-        }
-}
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
 
-Console.WriteLine($"всего {myArray.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
-Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
 */
